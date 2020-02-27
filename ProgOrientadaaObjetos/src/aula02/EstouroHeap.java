36a730943a7bdba0fa7df19ece96bf68f0cca421
Laboratorio 201 <Laboratorio 201@LAB201-16>
2020-02-27 17:53:41 -0300

    Classe estouro heap finalizada.

--- ProgOrientadaaObjetos/src/aula02/EstouroHeap.java
+++ ProgOrientadaaObjetos/src/aula02/EstouroHeap.java
@@ -1,9 +1,12 @@
 package aula02;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class EstouroHeap {
 	
 	public static class Node {
-		int[] vetor = new int[20000];
+		int[] vetor = new int[200000];
 		Node proximo;
 		public Node() {
 			
@@ -12,12 +15,12 @@ public class EstouroHeap {
 
 	public static void main(String[] args) {
 		System.out.println("Tentando estourar a memoria Heap");
-		//List<Node> nos = new ArrayList<>();
+		List<Node> nos = new ArrayList<>();
 		Node raiz = new Node();
 		Node n1 = raiz;
 		while (true) {
 			n1.proximo = new Node();
-			//nos.add(n1);
+			nos.add(n1);
 		}
 
 	}
